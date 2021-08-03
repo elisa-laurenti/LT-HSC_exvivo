6827a66ade0dce6dd3765647eb406eafeccb98ae
kendigsham <kendigbernard.sham@gmail.com>
2021-08-03 13:25:49 +0100

    DEGreport

--- s4_analyses_2/DEGreport_analysis/ss1_prepare_for_DEGreport.R
+++ s4_analyses_2/DEGreport_analysis/ss1_prepare_for_DEGreport.R
@@ -6,7 +6,7 @@ suppressPackageStartupMessages(library(DESeq2))
 
 dds <- readRDS('dds_noPD.rds')  ### deseq results from QA22 and qa81 combined with no PD cells
 
-result_list <- readRDS('deseq2_results_noPD.rds') ### deseq2 results between all timepoints
+unique_genes <- readRDS('DE_unique_genes.rds') 
 
 ######################
 #
@@ -26,21 +26,7 @@ ma <- limma::removeBatchEffect(ma,rld$batch)
 
 assay(rld) <- ma
 
-
-######################
-#
-#####################
-
-fdr<-0.05
-gene_list <- c()
-
-for (i in result_list){
-    i_Ordered <- i[!is.na(i$padj),]
-    filtered <- i_Ordered[i_Ordered$padj < fdr,]
-    gene_list <- c(gene_list, rownames(filtered))
-}
-unique_genes <- unique(gene_list)
-
+ 
 
 ######################
 #
